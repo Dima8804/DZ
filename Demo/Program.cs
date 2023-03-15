7a568c9a99b3c317491dbd05da65176fd94cc605
Dima Mynka <mdv8804@gmail.com>
2023-03-15 18:29:34 +0200

    Cod

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.Clear();
+
+int[] n = Console.ReadLine().Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+
+int[] result = Sum0(n);
+System.Console.WriteLine(result);
+
+int[] Sum0(int[] array)
+{
+    int[] res =new int [array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >=  100 & array[i] <= 1000)
+        {
+            res[i] = array[i];
+           System.Console.WriteLine( res[i]);
+        }
+
+    }
+    return res ;
+}
